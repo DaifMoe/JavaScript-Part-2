ded378c58e181558acc5bad5129205a51ba65d14
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-09 23:05:05 -0400

    When to use inheritance

---
+++ Prototypical Inheritance/WhenToUseInheritance.js
@@ -0,0 +1,6 @@
+// When you should use inheritance
+// Avoid creating inheritance hierarchies because they can be very fragile
+// Keep it to one level of inheritance do not go beyond that
+
+// Saying: Favor Composition over Inheritance
+// Use mixins to achieve compositions
