58731534cdc0359f60cc218c5b540ea9dedd37ab
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-10 10:49:21 -0400

    ES6 Tooling and Understanding Them

---
+++ ES6 Tooling/ES6Tooling.js
@@ -0,0 +1,4 @@
+// ES6 Tooling
+// When using modern JS we only need 2 tools a Transpiler and Bundler
+// Transpiler is a translator and compiler (converts JS code into code all browsers can understand)
+// Bunder (Webpack) remove white space and uglifies our code so that it takes up less space etc...
