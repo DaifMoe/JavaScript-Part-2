b108f591f0b8dbf630c5e1f09f12081febf1ab62
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-10 09:04:05 -0400

    The This Keyword

---
+++ ES6 Classes/TheThisKeyword.js
@@ -0,0 +1,24 @@
+// const Circle = function () {
+//   this.draw = function () {
+//     console.log(this);
+//   };
+// };
+
+// const c = new Circle();
+// // Method Call
+// c.draw();
+
+// const draw = c.draw;
+
+// // Function Call
+// draw();
+
+class Circle {
+  draw() {
+    console.log(this);
+  }
+}
+
+const c = new Circle();
+const draw = c.draw;
+draw();
